3dd90ec6f8303c8299c6c41dead0bd37c3886790
Irina <irinaadamova555@gmail.com>
2020-06-11 20:13:19 +0300

    try to connect to db

--- customers/settings.py
+++ customers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 #import customers.config as cnfg
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -89,6 +90,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
